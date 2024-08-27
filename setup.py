a165ef22b24bca12f4825ba3b671f486b1f5fd39
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:10:08 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_jacquesestimative',
-    py_modules=['cldfbench_jacquesestimative'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'jacquesestimative=cldfbench_jacquesestimative:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
